9f3f168747199059f675af3dda1e7b55e44ee63f
byLuisfer <luisferdass@gmail.com>
2025-03-26 15:19:34 +0100

    Created DTOs and Mapper for Users

---
+++ src/main/java/ch/scbe/productstore/resources/user/UserMapper.java
@@ -0,0 +1,22 @@
+package ch.scbe.productstore.resources.user;
+
+import ch.scbe.productstore.resources.user.dto.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public abstract class UserMapper {
+
+    @Mapping(source = "username", target = "username")
+    @Mapping(source = "password", target = "password")
+    public abstract Users toEntity(UserCreateDto userCreateDto);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "username", target = "username")
+    public abstract UserShowDto toShowDto(Users users);
+
+    @Mapping(source = "username", target = "username")
+    @Mapping(source = "password", target = "password")
+    public abstract void update(UserCreateDto userCreateDto, @MappingTarget Users usersToUpdate);
+}
